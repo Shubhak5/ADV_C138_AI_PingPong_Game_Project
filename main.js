d4396db6598acfd3b42b1992e3cde418222d4178
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-10-21 18:01:28 +0530

    ADV_C142_AI_PingPong_Game_Project
    Added JS code for starting the game

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ var ball = {
 var right_wrist_x = "";
 var right_wrist_y = "";
 var right_wrist_score = 0;
+var gameStatus = "";
 
 function setup() {
     var canvas = createCanvas(700, 600);
@@ -56,58 +57,61 @@ function gotPoses(results) {
 }
 
 function draw() {
+    if (gameStatus == "Start") {
+        background(0);
 
-    background(0);
+        image(video, 0, 0, 700, 600);
+        fill("black");
+        stroke("black");
+        rect(680, 0, 20, 700);
 
-    image(video, 0, 0, 700, 600);
-    fill("black");
-    stroke("black");
-    rect(680, 0, 20, 700);
+        fill("black");
+        stroke("black");
+        rect(0, 0, 20, 700);
 
-    fill("black");
-    stroke("black");
-    rect(0, 0, 20, 700);
+        //funtion paddleInCanvas call 
+        paddleInCanvas();
 
-    //funtion paddleInCanvas call 
-    paddleInCanvas();
-
-    //left paddle
-    fill(250, 0, 0);
-    stroke(0, 0, 250);
-    strokeWeight(0.5);
-    paddle1Y = mouseY;
-    rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
+        //left paddle
+        fill(250, 0, 0);
+        stroke(0, 0, 250);
+        strokeWeight(0.5);
+        paddle1Y = mouseY;
+        rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
 
 
-    //pc computer paddle
-    fill("#FFA500");
-    stroke("#FFA500");
-    var paddle2y = ball.y - paddle2Height / 2;
-    rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
+        //pc computer paddle
+        fill("#FFA500");
+        stroke("#FFA500");
+        var paddle2y = ball.y - paddle2Height / 2;
+        rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
 
-    //draw circle for right wrist
-    if (right_wrist_score > 0.2) {
-        fill("#FF0000");
-        stroke("#FF0000");
-        circle(right_wrist_x, right_wrist_y, 10);
+        //draw circle for right wrist
+        if (right_wrist_score > 0.2) {
+            fill("#FF0000");
+            stroke("#FF0000");
+            circle(right_wrist_x, right_wrist_y, 10);
+        }
+
+
+        //function midline call
+        midline();
+
+        //funtion drawScore call 
+        drawScore();
+
+        //function models call  
+        models();
+
+        //function move call which in very important
+        move();
     }
-
-
-    //function midline call
-    midline();
-
-    //funtion drawScore call 
-    drawScore();
-
-    //function models call  
-    models();
-
-    //function move call which in very important
-    move();
-
 }
 
-
+function startGame() {
+    gameStatus = "Start";
+    document.getElementById("status").innerHTML = "Game is Loaded";
+}
 
 //function reset when ball does notcame in the contact of padde
 function reset() {
